670c4a8a475d725ca60b20445fac3895a788feed
Levi Clements <levi.clements@outlook.com>
2026-02-03 16:49:40 -0700

    Adding .json files 	new file:   package-lock.json 	new file:   package.json

---
+++ javascript_test.js
@@ -0,0 +1,11 @@
+/* 
+Javascript is dynamically typed
+You don't have to declare the variable type
+console.log("Hello" + ' ' + 'world') prints to the terminal window
+
+Writing a function looks like:
+function join(a, b) {
+return a + ' ' + b;
+}
+
+*/
