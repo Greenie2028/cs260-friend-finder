15561af5fc3d30af6c087b45773adcf294aa3f15
Levi Clements <levi.clements@outlook.com>
2026-02-12 12:32:04 -0700

    Add final .jsx files and remove old html 	deleted:    account.html 	deleted:    create.html 	deleted:    friends.html 	deleted:    home.html 	deleted:    login.html 	modified:   src/account/account.jsx 	modified:   src/create/create.jsx

--- src/account/account.jsx
+++ src/account/account.jsx
@@ -2,8 +2,24 @@ import React from 'react';
 
 export function Account() {
   return (
-    <main className="container-fluid bg-secondary text-center">
-      <div>account displayed here</div>
-    </main>
+    <main>
+      <h3>User Name</h3>
+        <div>
+            <img src="placeholder.png" height="300px" width="275px" alt="Profile Picture"></img>
+        </div>
+        <h8>User Hometown</h8>
+        <p>placeholder@place.com</p>
+        <p>123-456-7890</p>
+        <div className="hobbies"></div>
+        <p><strong>My Hobbies</strong></p>
+        <ul>
+            <li>Hobby</li>
+            <li>Hobby</li>
+            <li>Hobby</li>
+            <li>Hobby</li>
+            <li>Hobby</li>
+        </ul>
+        <button className="btn btn-secondary btn-large" type="button" id="profile_button">Edit Profile</button>
+    </main> 
   );
 }
--- src/create/create.jsx
+++ src/create/create.jsx
@@ -2,8 +2,41 @@ import React from 'react';
 
 export function Create() {
   return (
-    <main className="container-fluid bg-secondary text-center">
-      <div>create displayed here</div>
+    <main>
+      <h3>Create account</h3>
+        <form method="get" action="home.html">
+            <div class="form">
+            <div>
+                <label for="email">Email:</label>
+                <input type="email" id="email" name="varEmail"></input>
+            </div>
+            <div>
+                <label for="password">Password:</label>
+                <input type="password" id="password" name="varPassword"></input>
+            </div>
+            <div>
+                <label for="confirm_pass">Confirm Password: </label>
+                <input type="password" id="confirm_pass" name="confirmPassword"></input>
+            </div>
+            <div>
+                <label for="name">Name:</label>
+                <input type="text" id="name" name="name"></input>
+            </div>
+            <div>
+            <label for="city">City:</label>
+            <input type="text" id="city" name="city"></input>
+            </div>
+            <div>
+            <label for="phone_num">Phone #: (Optional)</label>
+            <input type="tel" id="phone_num" name="phone"></input>
+            </div>
+            <div>
+            <label for="photo_id">Profile Photo</label>
+            <input type="file" id="photo_id" name="photo"></input>
+            </div>
+            </div>
+        <button class="btn btn-secondary" type="submit">Create Account</button>
+        </form>
     </main>
   );
 }
