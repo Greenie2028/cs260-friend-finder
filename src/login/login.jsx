a93977297293a36c999b7bbc64078968f821a683
Levi Clements <levi.clements@outlook.com>
2026-02-12 12:21:12 -0700

    Add login .jsx page 	modified:   src/login/login.jsx

--- src/login/login.jsx
+++ src/login/login.jsx
@@ -1,9 +1,27 @@
 import React from 'react';
+import '../app.css';
 
 export function Login() {
   return (
-    <main className="container-fluid bg-secondary text-center">
-      <div>login displayed here</div>
-    </main>
+    <main>
+        <form method="get" action="home.html">
+            <div className="form">
+                <h3>Sign in:</h3>
+                <label for="email">Email:</label>
+                <input type="email" id="email" name="varEmail" placeholder="example@email.com"></input>
+                <hr className="invis"></hr>
+                <label for="password">Password:</label>
+                <input type="password" id="password" name="varPassword" placeholder="password"></input>
+                <hr className="invis"></hr>
+            </div>
+            <button type="submit" className="btn btn-primary">Sign in</button>
+            </form>
+        <hr className="invis"></hr>
+        <a className="btn btn-primary" href="create.html">Create an account</a>
+        <hr className="invis"></hr>
+        <img src="friends_clip.png" alt="Hand Drawn Friends" width="30%"></img>
+        <hr className="invis"></hr>
+        <a href="http://www.freepik.com">Designed by Freepik</a>
+        </main>
   );
 }
