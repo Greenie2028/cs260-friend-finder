c1f6816371d42c8ed2853b90cf9b00ef25785b33
Levi Clements <levi.clements@outlook.com>
2026-02-12 12:23:37 -0700

    Add home.jsx 	modified:   src/home/home.jsx

--- src/home/home.jsx
+++ src/home/home.jsx
@@ -1,9 +1,33 @@
 import React from 'react';
+import '../app.css';
 
 export function Home() {
   return (
-    <main className="container-fluid bg-secondary text-center">
-      <div>home displayed here</div>
-    </main>
+    <main>
+        <div class="user_info">
+        <h3>Friend Name</h3>
+        <img src="placeholder.png" height="300px" width="275px" alt="Profile Picture"></img>
+        {/* I will pull location data finding nearby towns, not just the exact same town */}
+        <p>Note: I will use location data from a third party 
+            such as google to find nearby towns to the user</p>
+        <h8>Friend City</h8>
+        </div>
+        <div class="hobbies">
+        <p><b>Hobbies</b></p>
+        <ul>
+            <li>Hobby</li>
+            <li>Hobby</li>
+            <li>Hobby</li>
+            <li>Hobby</li>
+            <li>Hobby</li>
+        </ul>
+        </div>
+        <div>
+            <form>
+                <button class="btn btn-primary btn-sm" type="button">Not for me</button>
+                <button class="btn btn-primary btn-sm" type="button">Add Friend!!</button>
+            </form>
+        </div>
+        </main>
   );
 }
