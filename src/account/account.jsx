00b06e1c74e9c437ba44b85529a4cd243b802b0d
Levi Clements <levi.clements@outlook.com>
2026-02-25 17:04:40 -0700

    Add editing view 	modified:   src/account/account.jsx

--- src/account/account.jsx
+++ src/account/account.jsx
@@ -45,9 +45,25 @@ export function Account() {
       navigate('/login');
     }
 
+    if (!userData) return null;
+
   return (
     <main>
-      <h3>User Name</h3>
+      {editing ? (
+        <div className='form'>
+          <h3>Edit Profile</h3>
+          <label htmlFor="name">Name:</label>
+          <input type="text" id="name" value={name} onChange={(e) => setName(e.target.value)}/>
+          <label htmlFor="city">City:</label>
+          <input type="text" id="city" value={city} onChange={(e) => setCity(e.target.value)}/>
+          <label htmlFor="hobbies">Hobbies (comma seperated):</label>
+          <input type="text" id="hobbies" placeholder="Hiking, Reading, Biking" value={hobbies} onChange={(e) => setHobbies(e.target.value)}/>
+          <button className="btn-primary btn-sm" onClick={handleSave}>Save</button>
+          <button className="btn-secondary btn-sm" onClick={() => setEditing(false)}>Cancel</button>
+        </div>
+      ) : (
+      <>
+      <h3>{userData.name}</h3>
         <div>
             <img src="placeholder.png" height="300px" width="275px" alt="Profile Picture"></img>
         </div>
@@ -64,6 +80,8 @@ export function Account() {
             <li>Hobby</li>
         </ul>
         <button className="btn btn-secondary btn-large" type="button" id="profile_button">Edit Profile</button>
+        </>
+        )}
     </main> 
   );
 }
