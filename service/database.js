0e2cd4f1a38455de29b1fc36bd7fe4e17d786937
Levi Clements <levi.clements@outlook.com>
2026-03-24 21:27:32 -0600

    Added getUser function

--- service/database.js
+++ service/database.js
@@ -4,4 +4,15 @@ const config = require('./dbconfig.json');
 const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostname}`;
 
 const client = new MongoClient(url);
-const db = client.db()
+const db = client.db('friendfinder');
+
+async function connectToDatabase() {
+    await client.connect();
+    console.log('Connected to MongoDB');
+}
+
+connectToDatabase().catch(console.error);
+
+async function getUser(field, value) {
+    return db.collection('users').findOne({ [field]:value });
+}
